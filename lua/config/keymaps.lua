a47431f28955a51ee7c83beb91c30f2b3958c1de
Сухов Александр <asuhov@nic.ru>
2024-06-13 16:29:15 +0300

    shortcut for wa

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -37,3 +37,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-S>", "<cmd>wa<cr><esc>", { desc = "Save All Buffers" })
